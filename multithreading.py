92d5a8bb8b908365d4983f484bbb105dd82580ca
oblachko88 <88oblachko@gmail.com>
2023-09-07 13:23:12 +0200

    sep 7 13:23

--- multithreading.py
+++ multithreading.py
@@ -1,15 +1,26 @@
+# import threading
+
+# def function1():
+#   for x in range(20):
+#     print("ONE")
+
+# def function2():
+#   for x in range(20):
+#     print("TWO")
+
+# t1 = threading.Thread(target=function1)
+# t2 = threading.Thread(target=function2)
+
+# t1.start()
+# t2.start()
+
 import threading
+def hello():
+  for x in range(50):
+    print("Hello!")
 
-def function1():
-  for x in range(20):
-    print("ONE")
-
-def function2():
-  for x in range(20):
-    print("TWO")
-
-t1 = threading.Thread(target=function1)
-t2 = threading.Thread(target=function2)
-
+t1 = threading.Thread(target=hello)
 t1.start()
-t2.start()
+t1.join()
+
+print("Another text. ")
